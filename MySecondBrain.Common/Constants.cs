2608fd7770cce833aef683c261fdfedb1f9ccc11
JennyPans <jennypans@outlook.com>
2022-02-19 21:56:16 +0100

    Ajout d'une classe constante dans le module common

---
+++ MySecondBrain.Common/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySecondBrain.Common
+{
+    public class Constants
+    {
+        public const string ROLE_MEMBER = "Member";
+        public const string ROLE_ADMIN = "Admin";
+    }
+}
