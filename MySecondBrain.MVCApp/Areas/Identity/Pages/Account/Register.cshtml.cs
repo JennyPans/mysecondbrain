8e4d9f8f1571c21cb6efaa55751b974364aa72a9
JennyPans <jennypans@outlook.com>
2022-04-23 21:09:30 +0200

    Intégrer login et mot de passe aux users

--- MySecondBrain.MVCApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MySecondBrain.MVCApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -57,7 +57,7 @@ namespace MySecondBrain.MVCApp.Areas.Identity.Pages.Account
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
+            [Display(Name = "Re-type Password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
         }
